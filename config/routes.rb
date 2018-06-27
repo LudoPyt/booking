dc8a64cc226cf5362c672778282e7b6d18a0814f
Pouyaut Ludovic <pouyaut.ludovic@orange.fr>
2018-06-27 14:56:43 +0200

    add_routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
+resources :meets, except: [:edit, :update]
+namespace :admin do
+    resources :meets
+    resources :my, except: [:index]
+  end
+
   devise_for :users
+
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
