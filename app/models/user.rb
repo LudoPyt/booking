24f0d05aeafdd489f526ae71bc7552cb60884579
Pouyaut Ludovic <pouyaut.ludovic@orange.fr>
2018-06-27 16:13:20 +0200

    repair model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  has_many :meets
   validates :first_name, :last_name, presence:true
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
