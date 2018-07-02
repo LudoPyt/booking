eb82de5c0bff1c809c7998104ba9b9f5af3a2fd5
Pouyaut Ludovic <pouyaut.ludovic@orange.fr>
2018-07-02 17:32:51 +0200

    fifth commit test_admin

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_06_27_093721) do
+ActiveRecord::Schema.define(version: 2018_07_02_123004) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,6 +41,8 @@ ActiveRecord::Schema.define(version: 2018_06_27_093721) do
     t.datetime "date"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "first_name"
+    t.string "last_name"
     t.index ["user_id"], name: "index_meets_on_user_id"
   end
 
